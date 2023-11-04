485a81d19665ac4cb5dbbcc5546beda2b0f11d48
mikeyd234 <mikey_d234@hotmail.com>
2023-11-04 14:52:04 -0400

    Fixed index error when loading penguin dataset

--- penguins.py
+++ penguins.py
@@ -5,30 +5,29 @@ from sklearn.preprocessing import OneHotEncoder
 file_path = "C:/Users/Michael/source/repos/COMP472-A1/COMP472-A1-datasets/penguins.csv"
 penguins_data = pd.read_csv(file_path)
 
-# Display the first few rows of the dataset
+# Display all columns in the dataset
+pd.set_option('display.max_columns', None)
 print(penguins_data.head())
 
 # Method 1: Convert "island" and "sex" features into 1-hot vectors (dummy-coded data)
 # Create a copy of the original dataset to preserve the original data
 penguins_data_onehot = penguins_data.copy()
 
-# Use OneHotEncoder to convert "island" and "sex" into 1-hot vectors
-encoder = OneHotEncoder(sparse=False)
+# Create a OneHotEncoder for "island" and fit/transform it
+encoder_island = OneHotEncoder(sparse=False)
+island_encoded = encoder_island.fit_transform(penguins_data[['island']])
 
-# Transform "island" and "sex" columns into 1-hot encoded columns
-island_encoded = encoder.fit_transform(penguins_data[['island']])
-sex_encoded = encoder.fit_transform(penguins_data[['sex']])
+# Create a OneHotEncoder for "sex" and fit/transform it
+encoder_sex = OneHotEncoder(sparse=False)
+sex_encoded = encoder_sex.fit_transform(penguins_data[['sex']])
 
-# Create new column names for the 1-hot encoded columns
-island_categories = encoder.categories_[0]
-sex_categories = penguins_data['sex'].unique()
+# Get the feature names for the one-hot encoded columns
+island_feature_names = encoder_island.get_feature_names_out(['island'])
+sex_feature_names = encoder_sex.get_feature_names_out(['sex'])
 
-# Create new DataFrame for the 1-hot encoded columns
-island_df = pd.DataFrame(island_encoded, columns=[f'island_{cat}' for cat in island_categories])
-sex_df = pd.DataFrame(sex_encoded, columns=[f'sex_{cat}' for cat in sex_categories])
-
-# Reset the index of the island_df
-island_df = island_df.reset_index(drop=True)
+# Create DataFrames for the encoded features
+island_df = pd.DataFrame(island_encoded, columns=island_feature_names)
+sex_df = pd.DataFrame(sex_encoded, columns=sex_feature_names)
 
 # Concatenate the new 1-hot encoded columns with the original DataFrame
 penguins_data_onehot = pd.concat([penguins_data_onehot, island_df, sex_df], axis=1)
