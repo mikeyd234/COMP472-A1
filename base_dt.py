36bcb5c9ff4afb220e7bdf48c6272b6daa1865cb
Asha I <85359230+asha97@users.noreply.github.com>
2023-11-11 16:21:54 -0500

    modif

--- base_dt.py
+++ base_dt.py
@@ -2,14 +2,16 @@
 # Show the decision tree graphically 
 # for the abalone dataset, you can restrict the tree depth for visualisation purposes
 
-from sklearn.tree import DecisionTreeClassifier
-import pandas as pd
+from sklearn.tree import DecisionTreeClassifier #decision tree 
+from sklearn.model_selection import train_test_split #used in the splitting of validation and test data 
+import pandas as pd #file reader 
 
 # abalone data set 
 abalone_filepath = '/COMP472-A1-datasets/abalone.csv'
 abalone_data = pd.read_csv(abalone_filepath)
 
 # data: Type,LongestShell,Diameter,Height,WholeWeight,ShuckedWeight,VisceraWeight,ShellWeight,Rings
+# these are the target data 
 type_abalone = abalone_data['Type']
 longest_shell = abalone_data['LongestShell']
 diameter_abalone = abalone_data['Diameter']
@@ -19,4 +21,7 @@ shucked_weight = abalone_data['ShuckedWeight']
 viscera_weight = abalone_data['VisceraWeight']
 shell_weight = abalone_data['ShellWeight']
 rings_abalone = abalone_data['Rings']
+
+
+
 
